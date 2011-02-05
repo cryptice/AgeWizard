a260ee5db7d23c6e8ccf71e72d3fee2d353acf9f
Erik Lindblad <erik@l2c.se>
2011-02-05 23:09:45 +0100

    Age in months working.

--- lib/age_wizard.rb
+++ lib/age_wizard.rb
@@ -1,10 +1,14 @@
 module AgeWizard
   def self.age(d1, d2, output=:years)
+    year_diff = d2.year - d1.year
+    year_diff -= 1 if d2.month < d1.month || (d1.month == d2.month && d2.day < d1.day)
     case output
     when :years
-      tmp = d2.year - d1.year
-      tmp -= 1 if d2.month < d1.month || (d1.month == d2.month && d2.day < d1.day)
+      year_diff
+    when :months
+      month_diff = year_diff * 12
+      month_diff -= 1 if d2.day < d1.day
+      month_diff += d2.month - d1.month
     end
-    return tmp
   end
 end
--- spec/age_wizard_spec.rb
+++ spec/age_wizard_spec.rb
@@ -2,7 +2,7 @@ require "./lib/age_wizard"
 
 describe "Using age_wizard" do
 
-  it "should return correct number of years" do
+  it "should return correct age in years" do
     d1 = Time.utc(2009, 05, 15)
     AgeWizard::age(d1, Time.utc(2011, 07, 20)).should == 2
     AgeWizard::age(d1, Time.utc(2011, 03, 20)).should == 1
@@ -10,6 +10,15 @@ describe "Using age_wizard" do
     AgeWizard::age(d1, Time.utc(2011, 05, 10)).should == 1
     AgeWizard::age(d1, Time.utc(2011, 05, 15)).should == 2
     AgeWizard::age(d1, Time.utc(2011, 05, 20)).should == 2
+  end
+
+  it "should return correct age in months" do
+    d1 = Time.utc(2009, 05, 15)
+    AgeWizard::age(d1, Time.utc(2009, 06, 14), :months).should == 0
+    AgeWizard::age(d1, Time.utc(2009, 06, 15), :months).should == 1
+    AgeWizard::age(d1, Time.utc(2009, 06, 20), :months).should == 1
+    AgeWizard::age(d1, Time.utc(2009, 10, 20), :months).should == 5
+    AgeWizard::age(d1, Time.utc(2013, 10, 20), :months).should == 53
   end
 
 end
